ce1abcf9baef7f447ac225ea35060ccedfc0a4e5
sasha132461 <sasha1324612@gmail.com>
2023-07-08 09:44:16 +0300

    ))

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <cmath>
 
-unsigned long sum_squared(const int line) {
-    if (line < 0) {
+unsigned long sum_squared(const int line) 
+{
+    if (line < 0) 
+    {
         std::cout << "Invalid line number!" << std::endl;
         return 0;
     }
 
     unsigned long sum = 0;
     int a = 2;
-    int b = pow(a, line - 1);
+    int b = pow(a, line);
 
-    for (int i = 0; i <= line; i++) {
+    for (int i = 0; i <= line; i++) 
+    {
         sum = b;
     }
     
     return sum;
 }
 
-int main() {
+int main() 
+{
     int line;
     std::cout << "Enter the line number: ";
     std::cin >> line;
